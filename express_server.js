9656f6f974f1d10ea7e99fd91360ed7d24cb5f53
India Cornell <indiaccornell@gmail.com>
2023-07-29 16:15:31 -0700

    Add /urls endpoint

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs");// set the view engine to ejs
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,6 +19,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };// pass urlDatabase to templateVars
+  res.render("urls_index", templateVars);// pass templateVars to urls_index.ejs
 });
 
 app.listen(PORT, () => {
