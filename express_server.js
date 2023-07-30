648f6fe990c81a84f4c833b913dc01eec74309d7
India Cornell <indiaccornell@gmail.com>
2023-07-30 16:59:19 -0700

    add cookie POST in header, add POST endpoint, refactored formatting

--- express_server.js
+++ express_server.js
@@ -2,21 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");// set the view engine to ejs
+app.set("view engine", "ejs"); // set the view engine to ejs
 app.use(express.urlencoded({ extended: true }));
 
 function generateRandomString() {
   let randomString = "";
-  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for (let i = 0; i < 6; i++) {
-    randomString += possible.charAt(Math.floor(Math.random() * possible.length));
+    randomString += possible.charAt(
+      Math.floor(Math.random() * possible.length)
+    );
   }
   return randomString;
 }
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -32,8 +35,8 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };// pass urlDatabase to templateVars
-  res.render("urls_index", templateVars);// pass templateVars to urls_index.ejs
+  const templateVars = { urls: urlDatabase }; // pass urlDatabase to templateVars
+  res.render("urls_index", templateVars); // pass templateVars to urls_index.ejs
 });
 
 app.get("/urls/new", (req, res) => {
@@ -41,34 +44,44 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };// pass urlDatabase to templateVars
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  }; // pass urlDatabase to templateVars
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];//get the longURL from urlDatabase
-  console.log(longURL);//see the longURL
+  const longURL = urlDatabase[req.params.id]; //get the longURL from urlDatabase
+  console.log(longURL); //see the longURL
   res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();//generate a random string variable of 6 characters
-  urlDatabase[generateRandomString()] = req.body.longURL;//save the longURL into urlDatabase 
-  console.log(urlDatabase);//see the new urlDatabase
-  res.redirect(`/urls/${shortURL}`);//redirect to /urls/:shortURL
+  const shortURL = generateRandomString(); //generate a random string variable of 6 characters
+  urlDatabase[generateRandomString()] = req.body.longURL; //save the longURL into urlDatabase
+  console.log(urlDatabase); //see the new urlDatabase
+  res.redirect(`/urls/${shortURL}`); //redirect to /urls/:shortURL
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];//delete the longURL from urlDatabase
-  console.log(urlDatabase);//see the new urlDatabase
-  res.redirect("/urls");//redirect to /urls
+  delete urlDatabase[req.params.id]; //delete the longURL from urlDatabase
+  console.log(urlDatabase); //see the new urlDatabase
+  res.redirect("/urls"); //redirect to /urls
 });
 
 app.post("/urls/:id", (req, res) => {
-  const shortURL = req.params.id;//get the shortURL from urlDatabase
-  urlDatabase[shortURL] = req.body.newURL;//save the newURL into urlDatabase
-  console.log(urlDatabase);//see the new urlDatabase  
-  res.redirect("/urls");//redirect to /urls
+  const shortURL = req.params.id; //get the shortURL from urlDatabase
+  urlDatabase[shortURL] = req.body.newURL; //save the newURL into urlDatabase
+  console.log(urlDatabase); //see the new urlDatabase
+  res.redirect("/urls"); //redirect to /urls
+});
+
+app.post("/login", (req, res) => {
+  //set the username cookie <currently not seeing the username cookie>
+  const username = req.body.username; //get the username from req.body
+  res.cookie("username", username); //set the username cookie
+  res.redirect("/urls"); //redirect to /urls
 });
 
 app.listen(PORT, () => {
